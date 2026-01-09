af294d8a1a0b5cd9b81c616cf3d94e4eadab9d97
Tona <tona.chheun@akilabs.co>
2026-01-09 14:15:02 +0700

    add

--- src/datascience/main.py
+++ src/datascience/main.py
@@ -1,7 +1,15 @@
 import datascience.simple as helper1
+from sympy import symbols
+from sympy.plotting import plot3d
 
 def main():
     print("Hello World")
     print(2**3) # prints 8
     helper1.interest()
     helper1.interest2()
+
+    x, y = symbols('x y')
+    f = 2 * x + 3 * y
+
+    plot3d(f)
+    #plot3d(f, (x, -5, 5), (y, -5, 5))
